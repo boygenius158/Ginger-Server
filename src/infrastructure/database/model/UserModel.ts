758fd805c94244029423c0be93e02a6cc7185584
albinshiju <albindamn@gmail.com>
2024-09-21 19:56:16 +0530

    removed profile picture image

--- src/infrastructure/database/model/UserModel.ts
+++ src/infrastructure/database/model/UserModel.ts
@@ -25,7 +25,7 @@ const UserSchema: Schema<IUser> = new Schema({
     email: { type: String, required: true },
     password: { type: String, default: null },
     name: { type: String, default: "" },
-    profilePicture: { type: String, default: "https://instagram.fhyd14-1.fna.fbcdn.net/v/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=instagram.fhyd14-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=cXqyMerIMHAQ7kNvgFNYC7q&edm=AHBgTAQBAAAA&ccb=7-5&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.2-ccb7-5&oh=00_AYCTxqKdtDsgyzMlJw6nRF9R1G5OaVRA0O8liXsbuWJlNg&oe=66A84C8F&_nc_sid=21e75c" },
+    profilePicture: { type: String, default: "" },
     username: { type: String, default: "" },
     roles: { type: String, enum: Object.values(UserRole), default: UserRole.User },
     bio: { type: String, default: "" },
